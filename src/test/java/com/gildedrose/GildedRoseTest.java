84b5d7987c90a627f332627a65841982ca97b853
ahmad.alomary <ahmad.alomary@autotrader.co.uk>
2017-06-09 08:39:24 +0100

    efsdz

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,39 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void agedBrieValueIncreasesAsTimeGoesOn(){
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(7, app.items[0].quality);
+    }
+
+    @Test
+    public void backstagePassShouldWorkPlease1(){
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 1, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(33, app.items[0].quality);
+    }
+
+    @Test
+    public void backstagePassShouldWorkPlease2(){
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 7, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(32, app.items[0].quality);
+    }
+
+    @Test
+    public void backstagePassShouldWorkPlease3(){
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(30, app.items[0].quality);
     }
 
 }
